3adbe2041e4147fefa2cb214ccd07a5c07ea82d3
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-07-09 15:42:49 +0600

    Created task01.sh - script for creating new directories

---
+++ task01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir projects
+cd projects
+mkdir my_project
+cd my_project
+mkdir directory1 directory2 directory3
+cd directory1
+mkdir subdirectory1
+cd ..
+cd directory3
+mkdir subdirectory2
